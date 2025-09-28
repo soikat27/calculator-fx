df9df384970c6b7e7e9888238561748e743355d6
Soikat <193371447+soikat27@users.noreply.github.com>
2025-09-28 15:54:09 -0400

    Implement Action logic, multi-digit, operate
    -- Add button functionalities
    -- Add ButtonHandler to append numeric input to screen and num string
    -- Implement operator buttons with pushMultiDigit() and operate()
    -- Add pushMultiDigit() and operate() logic for stack-based calculation

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,6 +1,8 @@
 import java.util.Stack;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -9,7 +11,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
-import operators.Operator;
+import operators.*;
 
 public class Calculator extends Application {
     
@@ -23,6 +25,9 @@ public class Calculator extends Application {
     // stacks
 	private Stack<Operator> operator;
 	private Stack<Double>   operand;
+
+    // operand string
+	private String num;
 
     // the calculator buttons
 	private Button button0;
@@ -86,6 +91,38 @@ public class Calculator extends Application {
 		operator = new Stack<Operator> ();
 		operand  = new Stack<Double> ();
 
+        // instantiate the string operand
+		num = "";
+
+        // attach a handler to process button clicks
+		ButtonHandler handler = new ButtonHandler();
+
+		button0.setOnAction(handler);
+		button1.setOnAction(handler);
+		button2.setOnAction(handler);
+		button3.setOnAction(handler);
+		button4.setOnAction(handler);
+		button5.setOnAction(handler);
+		button6.setOnAction(handler);
+		button7.setOnAction(handler);
+		button8.setOnAction(handler);
+		button9.setOnAction(handler);
+
+		buttonPlus.setOnAction(handler);
+		buttonMinus.setOnAction(handler);
+		buttonMult.setOnAction(handler);
+		buttonDiv.setOnAction(handler);
+
+		buttonExp.setOnAction(handler);
+
+		buttonLPar.setOnAction(handler);
+		buttonRPar.setOnAction(handler);
+
+		buttonDot.setOnAction(handler);
+
+		buttonEqual.setOnAction(handler);
+		buttonClear.setOnAction(handler);
+
         // setup a grid panel for the keypad
 		GridPane keypad = new GridPane();
 
@@ -136,6 +173,234 @@ public class Calculator extends Application {
 
 		primaryStage.show();
     }
+
+    // Handler for processing the button clicks
+	private class ButtonHandler implements EventHandler<ActionEvent>
+	{ 
+		@Override 
+		public void handle(ActionEvent e) 
+		{
+			if (e.getSource() == button0)
+			{
+
+				screen.appendText("0");
+
+				num += "0";
+			}
+			else if (e.getSource() == button1)
+			{
+
+				screen.appendText("1");
+
+				num += "1";
+			}
+			else if (e.getSource() == button2)
+			{
+
+				screen.appendText("2");
+
+				num += "2";
+			}
+			else if (e.getSource() == button3)
+			{
+
+				screen.appendText("3");
+
+				num += "3";
+			}
+			else if (e.getSource() == button4)
+			{
+
+				screen.appendText("4");
+
+				num += "4";
+			}
+			else if (e.getSource() == button5)
+			{
+
+				screen.appendText("5");
+
+				num += "5";
+			}
+			else if (e.getSource() == button6)
+			{
+
+				screen.appendText("6");
+
+				num += "6";
+			}
+			else if (e.getSource() == button7)
+			{
+
+				screen.appendText("7");
+
+				num += "7";
+			}
+			else if (e.getSource() == button8)
+			{
+
+				screen.appendText("8");
+
+				num += "8";
+			}
+			else if (e.getSource() == button9)
+			{
+
+				screen.appendText("9");
+
+				num += "9";
+			}
+
+
+			else if (e.getSource() == buttonPlus)
+			{
+
+				pushMultiDigit ();
+
+				screen.appendText("+");
+
+				Operator add = new AddOperator ();
+
+				operate (add);
+			}
+			else if (e.getSource() == buttonMinus)
+			{
+
+				pushMultiDigit ();
+
+				screen.appendText("-");
+
+				Operator subt = new SubtOperator ();
+
+				operate (subt);
+			}
+			else if (e.getSource() == buttonMult)
+			{
+
+				pushMultiDigit ();
+
+				screen.appendText("*");
+
+				Operator mult = new MultOperator ();
+
+				operate (mult);
+			}
+			else if (e.getSource() == buttonDiv)
+			{
+
+				pushMultiDigit ();
+
+				screen.appendText("/");
+
+				Operator div = new DivOperator ();
+
+				operate (div);
+			}
+
+
+			else if (e.getSource() == buttonExp)
+			{
+
+				pushMultiDigit ();
+
+				screen.appendText("^");
+
+				Operator exp = new ExpOperator ();
+
+				operate (exp);
+			}
+
+
+			else if (e.getSource() == buttonLPar)
+			{
+
+				screen.appendText("(");
+
+				Operator lPar = new LeftParOperator ();
+
+				operator.push(lPar);
+			}
+			else if (e.getSource() == buttonRPar)
+			{
+
+				pushMultiDigit ();
+
+				screen.appendText(")");
+
+				Operator rPar = new RightParOperator ();
+
+				operate (rPar);
+
+				operator.pop(); // to get rid of right parenthesis
+				operator.pop(); // to get rid of left parenthesis
+			}
+
+
+			else if (e.getSource() == buttonDot)
+			{
+
+				screen.appendText(".");
+
+				num += ".";
+			}
+
+
+			else if (e.getSource() == buttonEqual)
+			{
+
+				pushMultiDigit ();
+
+				Operator equal = new EqualOperator ();
+
+				operate (equal);
+
+				screen.setText(String.valueOf(operand.peek()));
+
+				operator.clear();
+				operand.clear();
+			}
+			else if (e.getSource() == buttonClear)
+			{
+
+				num = "";
+
+				operand.clear();
+				operator.clear();
+
+				screen.clear();
+			}
+		}  
+	}
+
+    public void operate(Operator curOperator)
+	{
+		int curPrec = curOperator.getPrecedence();
+
+		while (!operator.isEmpty() && operator.peek().getPrecedence() >= curPrec)
+		{
+			Operator topStackOperator = operator.pop();
+
+			double b = operand.pop();
+			double a = operand.pop();
+
+			double intResult = topStackOperator.evaluate(a, b);
+
+			operand.push(intResult);
+		}
+
+		operator.push(curOperator);
+	}
+
+    public void pushMultiDigit()
+	{
+		if (num != "")
+		{
+			double curNumber = Double.parseDouble(num);
+
+			operand.push(curNumber);
+			num = "";
+		}
+	}
 
     public static void main(String[] args) 
     {
